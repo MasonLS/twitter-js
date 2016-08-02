02178b59f5ecfcc889be17ea447325c176d5b7ac
Mason Seale <masonseale88@gmail.com>
2016-08-02 16:48:12 -0400

    lala

--- routes/index.js
+++ routes/index.js
@@ -9,15 +9,23 @@ router.use(express.static('./public'));
 // 	res.sendFile(__dirname + '/stylesheets/style.css');
 // });
 
-
-
-
 router.get('/', function (req, res) {
   var tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets } );
+  res.render( 'index', { tweets: tweets, showForm: true } );
 });
 
+router.get('/users/:name', function(req, res) {
+	var name = req.params.name;
+	// var nameFinder = new RegExp(name, 'gi');
+	// var list = tweetBank.find( function(ele) {return nameFinder.test(ele['name'])} );
+    var list = tweetBank.find( {name: name} );
+	res.render( 'index', { tweets: list } );
+});
 
-
+router.get('/tweets/:id', function(req, res) {
+	var id = +req.params.id;
+	var rightTweet = tweetBank.find( {id: id} );
+	res.render( 'index', { tweets: rightTweet });
+});
 
 module.exports = router;
